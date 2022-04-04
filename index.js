f1828c0a69b176e6bb6af9ec03a460c3e65dab35
= <=>
2022-04-04 18:17:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var marks = [23, 45, 67, 78, 91];
+        const method = marks.map(x => x+10);
+        console.log(method);
+        const mfeach = marks.forEach((x,index) => {console.log (x+10)});
